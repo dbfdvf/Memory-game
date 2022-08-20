6d31f47ba2de882a7e7969b724155f7f0eef2d93
Pankla10 <dawid10cd@gmail.com>
2022-08-20 17:56:11 +0200

    feat: add mechanics checkmatched cards

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const board = document.querySelector(".board");
 
 class Board {
   constructor() {}
-  static words = ["www", "ddd", "eee", "hhh"];
+  static words = ["www", "ddd", "ddd", "hhh"];
+  static cards;
 
   static generateBoard() {
     const wordsShuffle = this.words.sort(() => Math.random() - 0.5);
-    console.log(wordsShuffle);
     for (let i = 0; i < 4; i++) {
       const div = document.createElement("div");
       div.textContent = wordsShuffle[i];
@@ -14,6 +14,45 @@ class Board {
       board.appendChild(div);
     }
   }
+
+  static addMechanicsToCard() {
+    let flag = 0;
+    this.cards = document.querySelectorAll(".card");
+    this.cards.forEach(function (card) {
+      card.addEventListener("click", function () {
+        card.classList.add("cardFlip");
+        card.classList.add("match");
+        flag++;
+        if (flag == 2) {
+          GameRules.checkTwoCards();
+          flag = 0;
+        }
+      });
+    });
+  }
+}
+
+class GameRules {
+  static checkTwoCards() {
+    let cardsMatch = document.querySelectorAll(".match");
+    function removeClasses() {
+      cardsMatch.forEach((card) => {
+        card.classList.remove("match");
+        card.classList.remove("cardFlip");
+      });
+    }
+    if (cardsMatch[0].textContent == cardsMatch[1].textContent) {
+      console.log("Dobrze");
+      setTimeout(removeClasses, 1000);
+      cardsMatch.forEach((card) => {
+        card.parentElement.removeChild(card);
+      });
+    } else {
+      console.log("źle");
+      setTimeout(removeClasses, 1000);
+    }
+  }
 }
 
 Board.generateBoard();
+Board.addMechanicsToCard();
